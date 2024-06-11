9a2ac22e2fe588592e206d17db0a27fd1f572cc5
cocoanautz <isaacong1@hotmail.com>
2024-06-11 13:06:39 +0800

    Create view for Patients

---
+++ api/views/patient_view.py
@@ -0,0 +1,53 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from api.models import Patient
+from api.serializers import PatientSerializer
+
+class PatientView(APIView):
+
+    def get(self, request):
+        patient_name = request.query_params.get("name")
+        patients = None
+
+        try:
+            if patient_name is None:
+                patients = Patient.objects.all()
+            else:
+                patients = Patient.objects.filter(name=patient_name)
+            
+            if patients is None:
+                return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            serializer = PatientSerializer(patients, many=True)
+
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({"error": str(e)})
+
+    def post(self, request):
+        try:
+            serializer = PatientSerializer(request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": str(e)})
+
+    def delete(self, request, pk):
+        try:
+            patient = Patient.objects.get(pk=pk)
+            patient.delete()
+            return Response({"message": "Deleted successfully"})
+        except Exception as e:
+            return Response({"error": str(e)})
+
+
+    
+
+
+        
+    
+        
