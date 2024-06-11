49b40c478ce5f423140f6978ede18ad51e7006e1
Ng Kay Hian <e1113766@u.nus.edu>
2024-06-11 13:07:11 +0800

    refactor vitals

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,8 @@ from api import views
 urlpatterns = [
     path("medications", views.MedicationView.as_view()),
     path("medications/<int:pk>", views.MedicationView.as_view()),
+    path("vitals", views.VitalsView.as_view()),
+    path("vitals/<int:pk>", views.VitalsView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,1 +1,2 @@
 from .medication_view import MedicationView
+from .vitals_view import VitalsView
