551fe22a649bd0440d1e52222fbd626456d62022
cocoanautz <isaacong1@hotmail.com>
2024-06-13 13:04:47 +0800

    Add testcases for Patient view

--- api/tests/test_medication.py
+++ api/tests/test_medication.py
@@ -1,6 +1,5 @@
 from api.tests.test_setup import TestSetup
 
-
 class TestMedicationAPI(TestSetup):
     def test_medicine_list(self):
         response = self.client.get("/medications")
--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,8 @@ from api import views
 urlpatterns = [
     path("medications", views.MedicationView.as_view()),
     path("medications/<int:pk>", views.MedicationView.as_view()),
-    path("patients", views.PatientView.as_view()),
-    path("patients/<int:pk>", views.PatientView.as_view()),
+    path("patients", views.PatientView.as_view(), name='patients'),
+    path("patients/<int:pk>", views.PatientView.as_view(), name='patients_detail'),
     path("visits", views.VisitView.as_view()),
     path("visits/<int:pk>", views.VisitView.as_view()),
     path("vitals", views.VitalsView.as_view()),
