1093278ffb8910cb6be94ab9c2bade2ac355578b
davidgohzk <david.gohzhekai@gmail.com>
2024-06-13 15:02:39 +0800

    added more dummies values

--- api/tests/dummy.py
+++ api/tests/dummy.py
@@ -1,6 +1,34 @@
+patients_post_dummy = {
+    "village_prefix": "VPF",
+    "name": "patient_name",
+    "identification_number": "identification_number",
+    "contact_no": "contact_no",
+    "gender": "gender",
+    "date_of_birth": "2021-01-01",
+    "drug_allergy": "drug_allergy",
+    "picture": "image/upload/v1715063294/ghynewr4gdhkuttombwc.jpg",
+}
+
+visit_post_dummy = {
+    "patient": 1,
+    "date": "2021-01-01",
+    "status": "status",
+}
+
+vitals_post_dummy = {
+    "visit": 1,
+    "doctor": 1,
+}
+
+consult_post_dummy = {}
+
+diagnosis_post_dummy = {}
+
 medication_post_dummy = {
     "medicine_name": "medicine_name",
     "quantity": 1,
     "notes": "notes",
     "remarks": "remarks",
 }
+
+order_post_dummy = {}
