5edea9201d309d726a5ea776162f676651d521ec
davidgohzk <david.gohzhekai@gmail.com>
2024-06-13 14:59:59 +0800

    changed dummy variables

--- api/tests/dummy.py
+++ api/tests/dummy.py
@@ -1,4 +1,4 @@
-medication_dummy = {
+medication_post_dummy = {
     "medicine_name": "medicine_name",
     "quantity": 1,
     "notes": "notes",
--- api/tests/test_medication.py
+++ api/tests/test_medication.py
@@ -1,10 +1,10 @@
 from api.tests.test_setup import TestSetup
 from api.tests.utils import general_test_API
-from api.tests.dummy import medication_dummy
+from api.tests.dummy import post_medication_dummy
 
 
 class TestMedicationAPI(TestSetup):
     def test_API(self):
         general_test_API(
-            self, medication_dummy, "medications_list", "medications_detail"
+            self, post_medication_dummy, "medications_list", "medications_detail"
         )
