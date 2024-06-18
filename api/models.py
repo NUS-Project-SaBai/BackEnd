c7e820e4dc7609d55ff80d8da3de441f78f35692
davidgohzk <david.gohzhekai@gmail.com>
2024-06-18 17:11:47 +0800

    refactored records-vitals

--- api/models.py
+++ api/models.py
@@ -35,18 +35,18 @@ class Vitals(models.Model):
     visit = models.OneToOneField(
         Visit, on_delete=models.SET_NULL, blank=True, null=True
     )
-    height = models.DecimalField(decimal_places=2, max_digits=5, default=0)
-    weight = models.DecimalField(decimal_places=2, max_digits=5, default=0)
-    systolic = models.IntegerField(default=0)
-    diastolic = models.IntegerField(default=0)
-    temperature = models.DecimalField(decimal_places=2, max_digits=5, default=0)
+    height = models.DecimalField(decimal_places=2, max_digits=5, null=True)
+    weight = models.DecimalField(decimal_places=2, max_digits=5, null=True)
+    systolic = models.IntegerField(null=True)
+    diastolic = models.IntegerField(null=True)
+    temperature = models.DecimalField(decimal_places=2, max_digits=5, null=True)
     diabetes_mellitus = models.TextField(
         blank=True, null=True, default="Haven't Asked / Not Applicable"
     )
-    heart_rate = models.IntegerField(default=0)
+    heart_rate = models.IntegerField(null=True)
     urine_test = models.TextField(blank=True, null=True)
-    hemocue_count = models.DecimalField(decimal_places=2, max_digits=5, default=0)
-    blood_glucose = models.DecimalField(decimal_places=2, max_digits=5, default=0)
+    hemocue_count = models.DecimalField(decimal_places=2, max_digits=5)
+    blood_glucose = models.DecimalField(decimal_places=2, max_digits=5)
     left_eye_degree = models.TextField(blank=True, null=True)
     right_eye_degree = models.TextField(blank=True, null=True)
     left_eye_pinhole = models.TextField(blank=True, null=True)
