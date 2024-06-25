40077833858eeb7e7e8a7c9bc59dfab2eff8a8b4
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-06-25 10:09:09 +0800

    fix databases setting name

--- sabaibiometrics/settings.py
+++ sabaibiometrics/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     # )
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_DB"),
+        "NAME": os.getenv("POSTGRES_NAME"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
