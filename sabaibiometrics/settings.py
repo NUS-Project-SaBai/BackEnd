dcb9d4e359ebb3abcb4ff1e2aca67e40a152e573
cocoanautz <isaacong1@hotmail.com>
2024-07-04 16:22:33 +0800

    Update pipfile to include boto3

--- sabaibiometrics/settings.py
+++ sabaibiometrics/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
     # )
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_NAME"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
