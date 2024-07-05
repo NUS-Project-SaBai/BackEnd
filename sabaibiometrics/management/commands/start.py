9e36a98011ef37572c757cce0c8a9a5859aa0bef
davidgohzk <david.gohzhekai@gmail.com>
2024-07-05 17:13:15 +0800

    updated commands

--- sabaibiometrics/management/commands/start.py
+++ sabaibiometrics/management/commands/start.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 import subprocess
 
 
@@ -7,7 +6,7 @@ class Command(BaseCommand):
     help = "Create default users"
 
     def handle(self, *args, **kwargs):
-        subprocess.run(["python", "manage.py", "makemigrations"])
-        subprocess.run(["python", "manage.py", "migrate"])
-        subprocess.run(["python", "manage.py", "set_auth0_users"])
-        subprocess.run(["python", "manage.py", "runserver"])
+        subprocess.run("python manage.py makemigrations", shell=True, text=True)
+        subprocess.run("python manage.py migrate", shell=True, text=True)
+        subprocess.run("python manage.py set_auth0_users", shell=True, text=True)
+        subprocess.run("python manage.py runserver", shell=True, text=True)
