3e5024216b4a972a55cba8a388bfb49f29141c0c
Dylan Khoo <dylankhoo@gmail.com>
2024-07-20 18:03:28 +0800

    - added POOR and BS2 fields for patient - modified patient fields to allow null values - Added unspecified as a gender option - Added UN as a village option (to represent unspecified) - modified patient_serializer accordingly

--- api/models/patient_model.py
+++ api/models/patient_model.py
@@ -7,13 +7,19 @@ class Patient(models.Model):
     class Meta:
         db_table = "patients"
 
-    village_prefix = models.CharField(max_length=5)
+    village_prefix = models.CharField(max_length=11)
     name = models.CharField(max_length=255)
     identification_number = models.CharField(
         max_length=255, blank=True, null=True)
-    contact_no = models.CharField(max_length=255)
-    gender = models.CharField(max_length=6)
+    contact_no = models.CharField(max_length=255, blank=True, null=True)
+    gender = models.CharField(max_length=11)
     date_of_birth = models.DateTimeField(default=timezone.now)
+    poor =  models.CharField(max_length=3, default="No")
+    bs2 =  models.CharField(max_length=3, default="No")
     drug_allergy = models.TextField(default="None")
     face_encodings = models.CharField(max_length=3000, blank=True, null=True)
-    picture = CloudinaryField("image")
+    picture = CloudinaryField("image", blank=True, null=True) 
+
+    # fix picture field accept no response
+    # poor = models.BooleanField()
+    # bs2 = models.BooleanField()
