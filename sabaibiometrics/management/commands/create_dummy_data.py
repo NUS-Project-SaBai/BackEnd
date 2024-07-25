9f10934952a782066483178b447435fc90e60522
ltfrxx <lutfir.rn@gmail.com>
2024-07-25 17:53:13 +0800

    Updated create_dummy_data.py

--- sabaibiometrics/management/commands/create_dummy_data.py
+++ sabaibiometrics/management/commands/create_dummy_data.py
@@ -11,31 +11,35 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         try:
-
+            '''
             #Create a User for the Consult doctor if it doesnt exist. This ensures
             #that a doctor is always available for creating related 'consult' 
             #records; reliabily referenced to the same doc. 
-            
+            '''
+
+            # Get first user available
             doctor = CustomUser.objects.get(pk=1)
             
 
-            #Number of patients and visits(max: 9) to create 
+            # Number of patients and visits(max: 9) to create 
             num_patients = 100 
             visits_per_patient = 3 
-            #loop to create multiple patients 
+            
+            # loop to create multiple patients 
             for i in range(num_patients):
                 patient_number = i + 1 
-                
+        
                 patient_name = f"Patient_{patient_number}"
                 identification_number = f"ID_{patient_number}"
                 contact_no = f"123456789{patient_number}"
                 gender = (["Male","Female"][patient_number % 2])
-                date_of_birth = f"199{patient_number%10}-01-01"
-                drug_allergy = (["None","Penicillin","Aspirin"][patient_number % 3])
+                date_of_birth = f"{patient_number%100 + 1000}-01-01"
+                drug_allergy = f"Allergy Medication {patient_number}"
+                village_prefix = (["PC", "CA", "TT", "TK", "SV"][(patient_number - 1) // (num_patients // 5)])
 
-                #Created a patient and assign it to the 'patient' variable 
+                # Created a patient and assign it to the 'patient' variable 
                 patient = Patient.objects.create(
-                    village_prefix= "VPF",
+                    village_prefix= village_prefix,
                     name= patient_name,
                     identification_number= identification_number,
                     contact_no= contact_no,
@@ -44,7 +48,7 @@ class Command(BaseCommand):
                     drug_allergy= drug_allergy,
                     picture= "image/upload/v1715063294/ghynewr4gdhkuttombwc.jpg",
                 )
-                #nested loop to create multiple visits per patient  
+                # nested loop to create multiple visits per patient  
                 for j in range(visits_per_patient):
                     visit_number = j + 1
                     
@@ -59,7 +63,7 @@ class Command(BaseCommand):
 
                     vitals_number = patient_number + (visit_number / 10)
                     
-                    #Create vitals and link to the 'visit'
+                    # Create vitals and link to the 'visit'
                     Vitals.objects.create(
                         visit= visit,
                         height= vitals_number,
@@ -68,7 +72,7 @@ class Command(BaseCommand):
                         diastolic= vitals_number,
                         temperature= vitals_number,
                         diabetes_mellitus= (["Yes","No"][visit_number % 2]),
-                        heart_rate= visit_number,
+                        heart_rate= vitals_number,
                         urine_test= (["True","False"][visit_number % 2]),
                         hemocue_count= vitals_number,
                         blood_glucose= vitals_number,
@@ -76,40 +80,40 @@ class Command(BaseCommand):
                         right_eye_degree= f"+{vitals_number}",
                         left_eye_pinhole= f"+{vitals_number}",
                         right_eye_pinhole= f"+{vitals_number}",
-                        others= "others",
+                        others= f"others{vitals_number}",
                     )
-                    #Create consult and link it to the 'visit' and 'doctor'
+                    # Create consult and link it to the 'visit' and 'doctor'
                     consult = Consult.objects.create(
                         visit= visit,
                         date= timezone.now(),
                         doctor= doctor, #Reference the doctor
-                        past_medical_history= (["None","Hypertension","Diabetes"][visit_number % 3]),
-                        consultation= "General checkup",
-                        plan= "Follow-up in 6 months",
-                        referred_for= "referred_for",
-                        referral_notes= "referral_notes",
-                        remarks= "remarks",
+                        past_medical_history= f"Medical Diagnosis {vitals_number}",
+                        consultation= f"General Checkup {vitals_number}",
+                        plan= f"Follow-up in {vitals_number} months",
+                        referred_for= f"referred_for_{vitals_number}",
+                        referral_notes= f"referral_notes_{vitals_number}",
+                        remarks= f"remark_{vitals_number}"
                     )
 
-                    #Create a diagnosis and link it to the 'consult'
+                    # Create a diagnosis and link it to the 'consult'
                     Diagnosis.objects.create(
                         consult= consult, 
-                        details= "consult_details",
-                        category= "consult_category",
+                        details= f"consult_details{vitals_number}",
+                        category= f"consult_category{vitals_number}",
                     )
-                    #Create medication 
+                    # Create medication 
                     medication = Medication.objects.create(
-                        medicine_name= random.choice(["Aspirin","Ibuprofen","Paracetamol"][visit_number % 3]),
+                        medicine_name= f"Medication {vitals_number}",
                         quantity= patient_number,
-                        notes= "Take as needed"
+                        notes= f"Take {vitals_number} as needed"
                     )
-                    #Create an order and link it to the 'medication' and 'consult' 
+                    # Create an order and link it to the 'medication' and 'consult' 
                     Order.objects.create(
                         medicine= medication,
                         consult= consult,
                         quantity= patient_number,
-                        notes= "order_notes",
-                        remarks= "remarks",
+                        notes= f"order_notes{vitals_number}",
+                        remarks= f"remark_{vitals_number}",
                         order_status= (["Completed","Pending"][visit_number % 2]),
                     )
 
