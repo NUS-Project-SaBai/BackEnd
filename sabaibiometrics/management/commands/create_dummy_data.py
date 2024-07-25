278a7287fce1bfb02c4e3396d0c06206551a2b0b
ltfrxx <lutfir.rn@gmail.com>
2024-07-25 15:06:40 +0800

    Modified create_dummy_data, removed random

--- sabaibiometrics/management/commands/create_dummy_data.py
+++ sabaibiometrics/management/commands/create_dummy_data.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from api.models import Patient, Visit, Vitals, Consult, Diagnosis, Medication, Order
+from api.models import Patient, Visit, Vitals, Consult, Diagnosis, Medication, Order, CustomUser
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
 from django.utils import timezone
@@ -15,22 +15,24 @@ class Command(BaseCommand):
             #Create a User for the Consult doctor if it doesnt exist. This ensures
             #that a doctor is always available for creating related 'consult' 
             #records; reliabily referenced to the same doc. 
-            doctor, created = User.objects.get_or_create(
-                username="doctor"
-                defaults={"email":"doctor@example.com","password":"password"}
-            )
+            
+            doctor = CustomUser.objects.get(pk=1)
+            
 
-            #Number of patients and visits to create 
-            num_patients = 10 
-            vists_per_patient = 3 
+            #Number of patients and visits(max: 9) to create 
+            num_patients = 100 
+            visits_per_patient = 3 
             #loop to create multiple patients 
-            for i in range(num_patients): 
-                patient_name = f"Patient_{i+1}"
-                identification_number = f"ID_{i+1}"
-                contact_no = f"123456789{i+1}"
-                gender = random.choice(["Male","Female"])
-                date_of_birth = f"199{i%10}-01-01"
-                drug_allergy = random.choice(["None","Penicillin","Aspirin"])
+            for i in range(num_patients):
+                patient_number = i + 1 
+                
+                patient_name = f"Patient_{patient_number}"
+                identification_number = f"ID_{patient_number}"
+                contact_no = f"123456789{patient_number}"
+                gender = (["Male","Female"][patient_number % 2])
+                date_of_birth = f"199{patient_number%10}-01-01"
+                drug_allergy = (["None","Penicillin","Aspirin"][patient_number % 3])
+
                 #Created a patient and assign it to the 'patient' variable 
                 patient = Patient.objects.create(
                     village_prefix= "VPF",
@@ -44,31 +46,36 @@ class Command(BaseCommand):
                 )
                 #nested loop to create multiple visits per patient  
                 for j in range(visits_per_patient):
-                    visit_date = f"2024-01-{j+1}"
-                    visit_status = random.choice(["Completed","Pending","Cancelled"])
+                    visit_number = j + 1
+                    
+                    visit_date = f"2024-01-{visit_number}"
+                    visit_status = (["Completed","Pending","Cancelled"][visit_number % 3])
 
                     visit = Visit.objects.create(
                         patient= patient,
                         date= visit_date,
                         status= visit_status,
                     )
-                    #Create vitals and link to the 'visit' 
+
+                    vitals_number = patient_number + (visit_number / 10)
+                    
+                    #Create vitals and link to the 'visit'
                     Vitals.objects.create(
                         visit= visit,
-                        height= round(random.uniform(100.0,200.0),2),
-                        weight= round(random.uniform(35.0,120.0),2),
-                        systolic= random.randint(90,140),
-                        diastolic= random.randint(60,90),
-                        temperature= round(random.uniform(35.0,40.0),1),
-                        diabetes_mellitus= random.choice(["Yes","No"]),
-                        heart_rate= random.randint(60,100),
-                        urine_test= random.choice(["True","False"]),
-                        hemocue_count= round(random.uniform(10.0,15.0),1),
-                        blood_glucose= round(random.uniform(70.0,120.0),1),
-                        left_eye_degree= f"+{random.randint(0,6)}",
-                        right_eye_degree= f"+{random.randint(0,6)}",
-                        left_eye_pinhole= f"+{random.randint(0,4)}",
-                        right_eye_pinhole= f"+{random.randint(0,4)}",
+                        height= vitals_number,
+                        weight= vitals_number, 
+                        systolic= vitals_number,
+                        diastolic= vitals_number,
+                        temperature= vitals_number,
+                        diabetes_mellitus= (["Yes","No"][visit_number % 2]),
+                        heart_rate= visit_number,
+                        urine_test= (["True","False"][visit_number % 2]),
+                        hemocue_count= vitals_number,
+                        blood_glucose= vitals_number,
+                        left_eye_degree= f"+{vitals_number}",
+                        right_eye_degree= f"+{vitals_number}",
+                        left_eye_pinhole= f"+{vitals_number}",
+                        right_eye_pinhole= f"+{vitals_number}",
                         others= "others",
                     )
                     #Create consult and link it to the 'visit' and 'doctor'
@@ -76,7 +83,7 @@ class Command(BaseCommand):
                         visit= visit,
                         date= timezone.now(),
                         doctor= doctor, #Reference the doctor
-                        past_medical_history= random.choice(["None","Hypertension","Diabetes"]),
+                        past_medical_history= (["None","Hypertension","Diabetes"][visit_number % 3]),
                         consultation= "General checkup",
                         plan= "Follow-up in 6 months",
                         referred_for= "referred_for",
@@ -92,19 +99,18 @@ class Command(BaseCommand):
                     )
                     #Create medication 
                     medication = Medication.objects.create(
-                        medicine_name= random.choice(["Aspirin","Ibuprofen","Paracetamol"]),
-                        quantity= random.randint(1,10),
-                        notes= "Take as needed",
-                        remarks= "common pain reliever",
+                        medicine_name= random.choice(["Aspirin","Ibuprofen","Paracetamol"][visit_number % 3]),
+                        quantity= patient_number,
+                        notes= "Take as needed"
                     )
                     #Create an order and link it to the 'medication' and 'consult' 
                     Order.objects.create(
                         medicine= medication,
                         consult= consult,
-                        quantity= random.randint(1,10),
+                        quantity= patient_number,
                         notes= "order_notes",
                         remarks= "remarks",
-                        order_status= random.choice(["Completed","Pending"]),
+                        order_status= (["Completed","Pending"][visit_number % 2]),
                     )
 
             self.stdout.write("Dummy data created successfully")
