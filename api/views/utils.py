773081a4d2d67db9e8dcfdc29ed68bf9412ad835
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-08-18 16:49:17 +0800

    removed offline

--- api/views/utils.py
+++ api/views/utils.py
@@ -1,6 +1,4 @@
 from sabaibiometrics.utils import jwt_decode_token
-from api.models import CustomUser
-from sabaibiometrics.settings import USE_DEFAULT_PERMISSION_CLASSES
 
 
 def get_doctor_id(headers):
@@ -9,6 +7,3 @@ def get_doctor_id(headers):
         payload = jwt_decode_token(token)
         doctor_id = payload.get("sub")
         return doctor_id
-    elif "doctor" in headers and not USE_DEFAULT_PERMISSION_CLASSES:
-        auth0_id = CustomUser.objects.get(email=headers["doctor"]).auth0_id
-        return auth0_id
