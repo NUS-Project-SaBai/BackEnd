6fdb6cbbeab3fe6c663a71c5913b870d5871869c
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-09-03 19:47:29 +0800

    "Try to remove requirements.txt"

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # Exit on error
 set -o errexit
 
+pip install pipenv
+pipenv requirements > requirements.txt
 pip install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py set_auth0_users
