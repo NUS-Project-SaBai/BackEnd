d4bb498c140b0c68012d3da3dafc0810222801c1
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-10-15 23:21:13 +0800

    Remove offline picture from patient serializer

--- api/serializers/patient_serializer.py
+++ api/serializers/patient_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from api import models
+from sabaibiometrics.settings import OFFLINE
 
 
 class PatientSerializer(serializers.ModelSerializer):
@@ -35,8 +36,7 @@ class PatientSerializer(serializers.ModelSerializer):
             "bs2": data["bs2"],
             "drug_allergy": data["drug_allergy"],
             "face_encodings": data["face_encodings"],
-            "picture": data["picture"],
-            "offline_picture": data["offline_picture"],
+            "picture": data["offline_picture"] if OFFLINE else data["picture"],
             "filter_string": self.get_patient_enriched(instance),
             "patient_id": self.get_patient_id(instance),
         }
