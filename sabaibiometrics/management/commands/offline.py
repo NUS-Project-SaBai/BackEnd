97269f0f5f1037c87abf791330278b773200f087
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-12-04 11:41:00 +0800

    Add offline command and readme

--- sabaibiometrics/management/commands/offline.py
+++ sabaibiometrics/management/commands/offline.py
@@ -6,4 +6,7 @@ class Command(BaseCommand):
     help = "Create default users"
 
     def handle(self, *args, **kwargs):
+        subprocess.run("python manage.py makemigrations", shell=True, text=True)
+        subprocess.run("python manage.py migrate", shell=True, text=True)
+        subprocess.run("python manage.py set_auth0_users", shell=True, text=True)
         subprocess.run("python manage.py runserver 0.0.0.0:8000", shell=True, text=True)
