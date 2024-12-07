04809dae318fc1661aa49f9181fc354b0892f81e
nathantew14 <nathantew14@gmail.com>
2024-12-08 01:47:24 +0700

    use env var for TELEGRAM_MESSAGE_THREAD_ID

--- api/middleware/tele_logging.py
+++ api/middleware/tele_logging.py
@@ -1,5 +1,5 @@
 import requests
-from sabaibiometrics.settings import TELEGRAM_API_TOKEN, TELEGRAM_CHAT_ID
+from sabaibiometrics.settings import TELEGRAM_API_TOKEN, TELEGRAM_CHAT_ID, TELEGRAM_MESSAGE_THREAD_ID
 
 class SimpleMiddleware:
     def __init__(self, get_response):
@@ -20,7 +20,7 @@ class SimpleMiddleware:
             if response.status_code != 200:
                 msg+=f"\n\nError:\n{response.content}"
             reply_url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage"    
-            data = {"chat_id": TELEGRAM_CHAT_ID, "message_thread_id":357, "text": msg.replace("<", "&lt;").replace(">", "&gt;").replace("&", "&amp")}
+            data = {"chat_id": TELEGRAM_CHAT_ID, "message_thread_id":TELEGRAM_MESSAGE_THREAD_ID, "text": msg.replace("<", "&lt;").replace(">", "&gt;").replace("&", "&amp")}
             requests.post(reply_url, data=data)
 
         # Code to be executed for each request/response after
