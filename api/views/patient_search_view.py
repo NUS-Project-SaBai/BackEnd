7ee89a63d2894e5669cc43c0da6f58b475118e62
nathantew14 <nathantew14@gmail.com>
2025-06-30 18:46:13 +0800

    dummy -> mock

--- api/views/patient_search_view.py
+++ api/views/patient_search_view.py
@@ -5,7 +5,7 @@ from random import randint
 from api.models import Patient
 from api.serializers import PatientSerializer
 from api.utils import facial_recognition
-from sabaibiometrics.settings import ENABLE_FACIAL_RECOGNITION, USE_DUMMY_FACIAL_RECOGNITION
+from sabaibiometrics.settings import ENABLE_FACIAL_RECOGNITION, USE_MOCK_FACIAL_RECOGNITION
 
 class PatientSearchView(APIView):
 
@@ -14,18 +14,18 @@ class PatientSearchView(APIView):
             face_encoding = facial_recognition.search_faceprint(request.data["picture"])
             ls = list(face_encoding.keys())
             patients = Patient.objects.filter(face_encodings__in=ls)
-        elif USE_DUMMY_FACIAL_RECOGNITION: # just select first 3 patients
+        elif USE_MOCK_FACIAL_RECOGNITION: # just select first 3 patients
             patients = Patient.objects.all()[0:3]
             face_encoding = {}
             for patient in patients:
                 print(patient.name, ": ", patient.face_encodings)
                 face_encoding[patient.face_encodings] = randint(1,99)/100
         else:
-            return Response("Actual and dummy facial recognition are both not enabled", 503)
+            return Response("Actual and mocked facial recognition are both not enabled", 503)
         serializer = PatientSerializer(
             patients, many=True, context={"confidence": face_encoding}
         )
         response = Response(serializer.data)
-        if USE_DUMMY_FACIAL_RECOGNITION:
-            response.headers["x-is-dummy-data"] = "True"
+        if USE_MOCK_FACIAL_RECOGNITION:
+            response.headers["x-is-mock-data"] = "True"
         return response
