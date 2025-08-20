c4d4b440f927f4025b5b7ebc0fd9cbc6a5dfcc14
Marcus Lim <marcuslim136@gmail.com>
2025-08-20 18:55:06 +0800

    remove pharmacy_stocks, no longer needed custom endpoint

--- api/routes/pharmacy_stocks.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from api.views.pharmacy_stocks_view import PharmacyStocksView
-
-urlpatterns = [
-    path("", PharmacyStocksView.as_view(), name="pharmacy_stocks"),
-]
--- api/services/pharmacy_stocks_service.py
+++
@@ -1,7 +0,0 @@
-from api.models import Medication
-from collections import defaultdict
-
-
-def get_pharmacy_stock_viewmodel():
-    medication_data = Medication.objects.all().values('id', 'medicine_name', 'quantity', 'code')
-    return list(medication_data)
