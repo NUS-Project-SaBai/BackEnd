4d74219e63737b63e7330b334b2525c7d668ec9b
Marcus Lim <marcuslim136@gmail.com>
2025-08-20 17:24:00 +0800

    fixed medical history flow from service to view model

--- api/serializers/pharmacy_stocks_serializer.py
+++ api/serializers/pharmacy_stocks_serializer.py
@@ -1,13 +1,5 @@
 from rest_framework import serializers
 
-class MedicationHistorySerializer(serializers.Serializer):
-    approval_name = serializers.CharField()
-    doctor_name = serializers.CharField()
-    patient_name = serializers.CharField()
-    qty_changed = serializers.IntegerField()
-    qty_remaining = serializers.IntegerField()
-    date = serializers.DateTimeField()
-
 class PharmacyStockSerializer(serializers.Serializer):
     medicine_id = serializers.IntegerField(source='id')
     medicine_name = serializers.CharField()
--- api/views/medication_history_view.py
+++ api/views/medication_history_view.py
@@ -2,11 +2,12 @@
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.services.pharmacy_stocks_service import get_medication_history_viewmodel
-from api.serializers.pharmacy_stocks_serializer import MedicationHistorySerializer
+from api.services.medication_history_service import get_medication_history_viewmodel
+from api.serializers.medication_history_serializer import MedicationHistorySerializer
 
 class MedicationHistoryView(APIView):
-    def get(self, request, medicine_id):
+    def get(self, request):
+        medicine_id = request.query_params.get("medicine_id")
         medication_history = get_medication_history_viewmodel(medicine_id)
         medication_history_serialized = MedicationHistorySerializer(medication_history, many=True)
         return Response(medication_history_serialized.data)
