73ed9b6a111a630e4a14f1509f0f39907d979f1a
Marcus Lim <marcuslim136@gmail.com>
2025-08-20 21:12:48 +0800

    fixed pharmacy orders service to base on visit

--- api/services/pharmacy_orders_service.py
+++ api/services/pharmacy_orders_service.py
@@ -15,22 +15,20 @@ def get_pharmacy_orders_viewmodel() -> List[PharmacyPatientBundleVM]:
         visit = consult.visit if consult else None
         patient = visit.patient if visit else None
     
-        pharmacy_orders_list = []
         diagnosis_list = []
-        
-        pharmacy_orders_list.append(
-            PharmacyOrderVM(order)
-        )
-        visit_diagnosis = Diagnosis.objects.filter(consult=consult).only("category", "details")
-        for diagnosis in visit_diagnosis:
-            diagnosis_list.append(DiagnosisVM(diagnosis))
-
         if patient not in patient_data_dict:
-            patient_data_dict[patient] = []
+            patient_data_dict[patient] = {}
         
-        patient_data_dict[patient].append(VisitBundleVM(visit, pharmacy_orders_list, diagnosis_list))
+        if visit not in patient_data_dict[patient]:
+            visit_diagnosis = Diagnosis.objects.filter(consult=consult).only("category", "details")
+            for diagnosis in visit_diagnosis:
+                diagnosis_list.append(DiagnosisVM(diagnosis))
+            
+            patient_data_dict[patient][visit] = VisitBundleVM(visit, [], diagnosis_list)
+    
+        patient_data_dict[patient][visit].orders.append(PharmacyOrderVM(order))
 
     final_viewmodel_list = []
-    for patient in patient_data_dict:
-        final_viewmodel_list.append(PharmacyPatientBundleVM(PatientHeaderVM(patient), patient_data_dict[patient]))
+    for patient, visit in patient_data_dict.items():
+        final_viewmodel_list.append(PharmacyPatientBundleVM(PatientHeaderVM(patient), list(patient_data_dict[patient].values())))
     return final_viewmodel_list
