687e1d9ffb454376806fab9f80a32bf868a3f852
Noah Seethor <noahseethor@gmail.com>
2025-08-24 01:36:05 +0800

    Rebased and updated .env.example

--- api/views/patient_search_view.py
+++ api/views/patient_search_view.py
@@ -4,26 +4,30 @@ from random import randint
 
 from api.models import Patient
 from api.serializers import PatientSerializer
-from api.utils import facial_recognition
-from sabaibiometrics.settings import ENABLE_FACIAL_RECOGNITION, USE_MOCK_FACIAL_RECOGNITION
+from api.services import patient_service
+from sabaibiometrics.settings import (
+    ENABLE_FACIAL_RECOGNITION,
+    USE_MOCK_FACIAL_RECOGNITION,
+)
+
 
 class PatientSearchView(APIView):
-
     def post(self, request):
         if ENABLE_FACIAL_RECOGNITION:
-            face_encoding = facial_recognition.search_faceprint(request.data["picture"])
-            ls = list(face_encoding.keys())
-            patients = Patient.objects.filter(face_encodings__in=ls)
-        elif USE_MOCK_FACIAL_RECOGNITION: # just select first 3 patients
+            picture = request.data["picture"]
+            patients, confidence_dict = patient_service.search_patients_by_face(picture)
+        elif USE_MOCK_FACIAL_RECOGNITION:  # just select first 3 patients
             patients = Patient.objects.all()[0:3]
-            face_encoding = {}
+            confidence_dict = {}
             for patient in patients:
-                print(patient.name, ": ", patient.face_encodings)
-                face_encoding[patient.face_encodings] = randint(1,99)/100
+                confidence_dict[patient.face_encodings] = randint(1, 99) / 100
         else:
-            return Response("Actual and mocked facial recognition are both not enabled", 503)
+            return Response(
+                "Actual and mocked facial recognition are both not enabled", 503
+            )
+
         serializer = PatientSerializer(
-            patients, many=True, context={"confidence": face_encoding}
+            patients, many=True, context={"confidence": confidence_dict}
         )
         response = Response(serializer.data)
         if USE_MOCK_FACIAL_RECOGNITION:
