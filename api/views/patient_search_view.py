6d92b45aa12fe37d7dd10059770ae0be94703261
nathantew14 <nathantew14@gmail.com>
2025-10-02 01:01:13 +0800

    Add last_visit to patient search by face

--- api/views/patient_search_view.py
+++ api/views/patient_search_view.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from random import randint
+from django.db.models import OuterRef, Subquery, DateField, IntegerField
 
 from api.models import Patient
+from api.models.visit_model import Visit
 from api.serializers import PatientSerializer
 from api.services import patient_service
 from sabaibiometrics.settings import (
@@ -12,6 +14,8 @@ from sabaibiometrics.settings import (
 
 
 class PatientSearchView(APIView):
+    patient_visits_qs = Visit.objects.filter(patient_id=OuterRef('pk')).order_by('-date')
+    
     def post(self, request):
         if ENABLE_FACIAL_RECOGNITION:
             picture = request.data["picture"]
@@ -25,6 +29,11 @@ class PatientSearchView(APIView):
             return Response(
                 "Actual and mocked facial recognition are both not enabled", 503
             )
+
+        patients = patients.annotate(
+            last_visit_date=Subquery(self.patient_visits_qs.values('date')[:1], output_field=DateField()),
+            last_visit_id=Subquery(self.patient_visits_qs.values('pk')[:1], output_field=IntegerField()),
+        )
 
         serializer = PatientSerializer(
             patients, many=True, context={"confidence": confidence_dict}
